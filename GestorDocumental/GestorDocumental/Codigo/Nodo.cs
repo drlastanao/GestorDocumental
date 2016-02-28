96ff9169cf14b9740105bef42c2bb898dca17040
drlastanao <drlastanao@gmail.com>
2016-02-28 22:00:16 +0100

    busquedas

---
+++ GestorDocumental/GestorDocumental/Codigo/Nodo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDocumental.Codigo
+{
+    [Serializable()]
+    public class Nodo
+    {
+        public String codigo { get; set; }
+        public String descripción { get; set; }
+        public String otros { get; set; }
+
+        public String enlace { get; set; }
+        public List<String> enlaces { get; set; }
+
+        public String carpeta { get; set; }
+        public List<String> subcarpetas { get; set; }
+
+
+
+
+        public bool buscar(String texto)
+        {
+            bool encontrado = false;
+
+            string[] palabras = texto.Split(' ');
+
+
+            foreach (var palabra in palabras)
+            {
+                encontrado = encontrado | Utiles.ContainsIgnore(codigo, texto) | Utiles.ContainsIgnore(descripción, texto)
+                                        | Utiles.ContainsIgnore(otros, texto) | Utiles.ContainsIgnore(enlace, texto) | Utiles.ContainsIgnore(carpeta, texto);
+
+                if (encontrado == false)
+                    encontrado = encontrado | buscarLista(enlaces, texto);
+
+                if (encontrado == false)
+                    encontrado = encontrado | buscarLista(subcarpetas, texto);
+
+
+                if (encontrado) break;
+
+            }
+
+
+
+            return encontrado;
+
+
+        }
+
+
+        //devuelve todos los enlaces o carpetas que tienen alguna de las palabras   (sin repetir)
+
+        public List<String> buscarNodosContienenTexto(String texto)
+        {
+            List<String> nodos = new List<String>();
+
+            string[] palabras = texto.Split(' ');
+
+
+            foreach (var palabra in palabras)
+            {
+                List<String> aux;
+
+                aux = nodosContienen(subcarpetas, palabra);
+                nodos.AddRange(aux);
+                aux = nodosContienen(enlaces, palabra);
+                nodos.AddRange(aux);
+            }
+
+            List<String> devolver = new List<String>();
+            devolver.AddRange(nodos.Distinct());
+            return devolver;
+
+        }
+
+
+        //busca en una lista el texto y dice si está o no
+        private bool buscarLista(List<string> lista, string texto)
+        {
+            foreach (var item in lista)
+                if (Utiles.ContainsIgnore(item.ToString(), texto))
+                    return true;
+
+            return false;
+
+
+        }
+
+        //devuelve los elementos de la lista que tienen el texto
+        private List<String> nodosContienen(List<string> lista, string texto)
+        {
+            List<String> devuelvelista = new List<string>();
+
+            foreach (var item in lista)
+                if (Utiles.ContainsIgnore(item.ToString(), texto))
+                    devuelvelista.Add(item.ToString());
+
+            return devuelvelista;
+
+        }
+    }
+}
+
---
+++ GestorDocumental/GestorDocumental/Codigo/Utiles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDocumental.Codigo
+{
+    public class Utiles
+    {
+
+        public static bool Contains(string source, string toCheck, StringComparison comp)
+        {
+            return source != null && toCheck != null && source.IndexOf(toCheck, comp) >= 0;
+        }
+
+
+        public static bool ContainsIgnore (string source, string toCheck)
+        {
+            return source!=null && toCheck != null && source.IndexOf(toCheck, StringComparison.CurrentCultureIgnoreCase) >= 0;
+        }
+
+    }
+
+}
+
